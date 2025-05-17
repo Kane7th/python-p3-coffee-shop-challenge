f41e4c55b2fe3c36790769fa0ee03d73cef80d38
Kane Kabena <onekaneldn@gmail.com>
2025-05-17 11:30:46 +0300

    added comments to explain each line of code thus far

--- modals/customer.py
+++ modals/customer.py
@@ -1,15 +1,28 @@
-from .order import Order
+from .order import Order # Importing the Order class from order module
 
-class Customer:
-    def __init__(self, name):
-        if not (1 <= len(name) <= 15):
-            raise ValueError("Name must be between 1 and 15 characters")
-        self.name = name
-        self.orders = []
+class Customer:  # Customer class to represent a customer
+    def __init__(self, name): # constructor to initialize the customer
+        self.name = name # sets the name using the setter
+        self.orders = [] # initializes an empty list of orders
 
-    def add_order(self, coffee, price):
-        order = Order(self, coffee, price)
-        self.orders.append(order)
 
-    def __str__(self):
-        return f"Customer name is: {self.name}"
+    @property  # getter simply returns the stored '_name'
+    def name(self): # returns the name of the customer
+        return self._name # returns the stored name in private variable
+    
+
+    @name.setter  # checks type and length, raising errors if invalid
+    def name(self, value):
+        if not isinstance(value, str): # checks if value is a string
+            raise ValueError("Name must be a string value")
+        if not (1 <= len(value) <= 15): # checks if length is between 1 and 15
+            raise ValueError("Name must be at least 3 characters long")
+        self._name = value  # actual value is stored in self._name
+
+
+    def add_order(self, coffee, price):   # method to add an order 
+        order = Order(self, coffee, price) # creates an order object
+        self.orders.append(order) # adds order to the list of orders
+
+    def __str__(self): # string representation of the customer
+        return f"Customer name is: {self.name}" # returns the name of the customer
